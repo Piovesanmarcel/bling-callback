aa17da68201c1ad3bcc31ca5064f6b782b75223b
Piovesanmarcel <piovesan.marcel@gmail.com>
2025-05-18 12:51:18 -0300

    Create refresh-bling-token.js

---
+++ api/refresh-bling-token.js
@@ -0,0 +1,5 @@
+import { createClient } from "@supabase/supabase-js";
+
+const supabase = createClient(
+  "https://bpqtzydsxmjazdzzcvno.supabase.co",
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBh
