e8460cf7048e6526995925908f2e7f87a7b0f99c
Piovesanmarcel <piovesan.marcel@gmail.com>
2025-05-18 12:24:35 -0300

    Update bling-callback.js

--- api/bling-callback.js
+++ api/bling-callback.js
@@ -5,17 +5,22 @@ export default async function handler(req, res) {
     return res.status(400).send("Faltando parâmetros.");
   }
 
+  // Montar autenticação básica com client_id e secret
+  const auth = Buffer.from(
+    "e010efc402b05a2023303c5d55a527571d0b8da3" + ":" + "a92dcc7a3552afa00af24b911e78000722c4b60d0cc822dd418698959caf"
+  ).toString("base64");
+
+  // Requisição para trocar o code pelo token de acesso do Bling
   const response = await fetch("https://www.bling.com.br/Api/v3/oauth/token", {
     method: "POST",
     headers: {
       "Content-Type": "application/x-www-form-urlencoded",
+      "Authorization": `Basic ${auth}`,
     },
     body: new URLSearchParams({
       grant_type: "authorization_code",
       code,
       redirect_uri: "https://bling-callback.vercel.app/api/bling-callback",
-      client_id: "e010efc402b05a2023303c5d55a527571d0b8da3",
-      client_secret: "a92dcc7a3552afa00af24b911e78000722c4b60d0cc822dd418698959caf",
     }),
   });
 
@@ -25,7 +30,7 @@ export default async function handler(req, res) {
     return res.status(500).json({ error: "Erro ao trocar token", details: data });
   }
 
-  console.log("Token Bling:", data);
+  console.log("TOKEN BLING:", data);
 
   return res.send("✅ Bling conectado com sucesso!");
 }
