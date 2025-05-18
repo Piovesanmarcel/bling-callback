b7113cdfed6920d241511e1210f0dd55f89c4d3c
Piovesanmarcel <piovesan.marcel@gmail.com>
2025-05-18 12:12:44 -0300

    Update bling-callback.js

--- api/bling-callback.js
+++ api/bling-callback.js
@@ -7,13 +7,15 @@ export default async function handler(req, res) {
 
   const response = await fetch("https://www.bling.com.br/Api/v3/oauth/token", {
     method: "POST",
-    headers: { "Content-Type": "application/x-www-form-urlencoded" },
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded",
+    },
     body: new URLSearchParams({
       grant_type: "authorization_code",
       code,
       redirect_uri: "https://bling-callback.vercel.app/api/bling-callback",
       client_id: "e010efc402b05a2023303c5d55a527571d0b8da3",
-      client_secret: "a92dcc7a3552afa00af24b911e78000722c4b60d0cc822dd418698959caf"
+      client_secret: "a92dcc7a3552afa00af24b911e78000722c4b60d0cc822dd418698959caf",
     }),
   });
 
@@ -23,7 +25,7 @@ export default async function handler(req, res) {
     return res.status(500).json({ error: "Erro ao trocar token", details: data });
   }
 
-  console.log("TOKEN BLING:", data);
+  console.log("Token Bling:", data);
 
   return res.send("✅ Bling conectado com sucesso!");
 }
