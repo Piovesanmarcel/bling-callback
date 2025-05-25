1c76ed2ae1347b981972e1cb4876c4bdd8cb67b5
Piovesanmarcel <piovesan.marcel@gmail.com>
2025-05-25 15:53:44 -0300

    Delete src directory

--- src/app/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <div style={{ padding: "2rem", fontFamily: "sans-serif" }}>
-      <h1>🧩 Bling Callback API</h1>
-      <p>Rota <code>/api/bling-callback</code> ativa.</p>
-    </div>
-  );
-}
