b45d856ae142fb402ff65e0a66151fd7f5caa62c
Piovesanmarcel <piovesan.marcel@gmail.com>
2025-05-25 15:28:52 -0300

    Create layout.tsx

---
+++ src/app/layout.tsx
@@ -0,0 +1,12 @@
+export const metadata = {
+  title: "Bling Callback",
+  description: "Integração com Bling ERP",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="pt-br">
+      <body>{children}</body>
+    </html>
+  );
+}
