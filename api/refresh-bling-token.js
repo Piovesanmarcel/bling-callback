1fe75bbfd05f7418ef9516c5a925d582555661de
Piovesanmarcel <piovesan.marcel@gmail.com>
2025-05-25 15:53:26 -0300

    Delete api directory

--- api/refresh-bling-token.js
+++
@@ -1,5 +0,0 @@
-import { createClient } from "@supabase/supabase-js";
-
-const supabase = createClient(
-  "https://bpqtzydsxmjazdzzcvno.supabase.co",
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBh
