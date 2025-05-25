a1016372ff6e367f7fa8585ab18566a2feae6213
Piovesanmarcel <piovesan.marcel@gmail.com>
2025-05-25 15:56:56 -0300

    Add files via upload

---
+++ src/app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ padding: "2rem", fontFamily: "sans-serif" }}>
+      <h1>🧩 Bling Callback API</h1>
+      <p>Rota <code>/api/bling-callback</code> ativa e funcionando.</p>
+    </div>
+  );
+}
